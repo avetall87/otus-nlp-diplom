d449b04a7d257127c735ba28f3830e7cbb3c57f3
avetall87 <aleksandrov.vetall@yandex.ru>
2023-05-24 01:19:00 +0300

    update retriver-service

--- retriver-service/model/__init__.py
+++ retriver-service/model/__init__.py
@@ -6,4 +6,4 @@ import numpy as np
 tokenizer = AutoTokenizer.from_pretrained("sberbank-ai/sbert_large_nlu_ru")
 model = AutoModel.from_pretrained("sberbank-ai/sbert_large_nlu_ru")
 df = pd.read_csv('./dataset/dataset.csv', sep=',', engine='python')
-data_vectors = np.load('./dataset/vectors.npy')
+data_vectors = np.load('./model/vectors.npy')
